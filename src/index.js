31d9475bed50c9e80702c1fb03857bfe91c8e293
Dimitar Petrov <60468978+DimitarPetrov1@users.noreply.github.com>
2021-02-16 09:48:18 -0800

    Removed unwanted functions

--- src/index.js
+++ src/index.js
@@ -1,81 +1,29 @@
 const numbers = document.querySelectorAll(".number");
+const buttons = document.querySelectorAll(".btn");
+let displayWinningNumber = document.getElementById("winningNumber");
 let winningNumbers = [];
 let selectedNumbers = [];
-const displayWinningNumber = document.getElementById("winningNumber");
-const displaySelectedNumbers = document.getElementById("selectedNumbers");
-const startGameBtn = document.getElementById("startGameBtn");
-// Alt buttons
-const firstTwelveBtn = document.getElementById("firstTwelveBtn");
-const secondTwelveBtn = document.getElementById("secondTwelveBtn");
-const thirdTwelveBtn = document.getElementById("thirdTwelveBtn");
-//
-const firstHalfBtn = document.getElementById("firstHalfBtn");
-const secondHalfBtn = document.getElementById("secondHalfBtn");
 
-//
-const firstTwelve = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-const secondTwelve = [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
-const thirdTwelve = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36];
-//
-const firstHalf = [
-  1,
-  2,
-  3,
-  4,
-  5,
-  6,
-  7,
-  8,
-  9,
-  10,
-  11,
-  12,
-  13,
-  14,
-  15,
-  16,
-  17,
-  18
-];
-const secondHalf = [
-  19,
-  20,
-  21,
-  22,
-  23,
-  24,
-  25,
-  26,
-  27,
-  28,
-  29,
-  30,
-  31,
-  32,
-  33,
-  34,
-  35,
-  36
-];
-
-const selectNumber = (e) => {
+const coin = document.createElement("div");
+coin.classList.add("coin");
+coin.textContent = "$";
+const removeCoinsBtn = document.createElement("div");
+removeCoinsBtn.classList.add("remove-coins-btn");
+removeCoinsBtn.textContent = "X";
+const selectSingleNumber = (e) => {
   // click functions to add selected class
   // get the target number + the target element
   // if the number is selected : push the target number in Array, add Selected class, and display result
   // if the number is deselected : remove Selected class, find index of target number, using splice delete it from Array, then display the selected numbers again
   let targetNumber = Number(e.target.textContent);
   let targetEl = e.target;
-  if (!targetEl.classList.contains("number-selected")) {
+  if (!targetEl.classList.contains("number-single-selected")) {
     selectedNumbers.push(targetNumber);
-    targetEl.classList.add("number-selected");
-    displaySelectedNumbers.textContent =
-      "You have selected: " + selectedNumbers;
+    targetEl.classList.add("number-single-selected");
   } else {
-    targetEl.classList.remove("number-selected");
+    targetEl.classList.remove("number-single-selected");
     let index = selectedNumbers.indexOf(targetNumber);
     selectedNumbers.splice(index, 1);
-    displaySelectedNumbers.textContent =
-      "You have selected: " + selectedNumbers;
   }
 };
 const startGame = () => {
@@ -84,7 +32,10 @@ const startGame = () => {
     let winningNumber = Math.floor(Math.random() * 37);
 
     for (let i = 0; i < numbers.length; i++) {
-      if (numbers[winningNumber].classList.contains("number-selected")) {
+      if (
+        numbers[winningNumber].classList.contains("number-selected") ||
+        numbers[winningNumber].classList.contains("number-single-selected")
+      ) {
         console.log("You win from " + winningNumber);
       } else {
         console.log("You loose");
@@ -97,110 +48,45 @@ const startGame = () => {
     console.log("Please select a number first");
   }
 };
-// 1st 12
-const firstTwelveSelect = () => {
-  let index = selectedNumbers.indexOf(firstTwelve);
-  // first add the classes where needed
-  for (let i = 1; i < 13; i++) {
-    // get numbers displayed on screen (red & black)
-    if (!numbers[i].classList.contains("number-selected")) {
-      numbers[i].classList.add("number-selected");
-    } else {
-      numbers[i].classList.remove("number-selected");
+
+const selectMultipleNumbers = (e) => {
+  let eventId = e.target.getAttribute("id"); // The ID of the clicked button
+  let eventTarget = e.target; // The target HTML element to add COINS
+  if (eventId === "firstTwelveBtn") {
+    for (let i = 1; i < 13; i++) {
+      // let index = selectedNumbers.indexOf(i);
+      if (!numbers[i].classList.contains("number-selected")) {
+        selectedNumbers.push(i);
+        numbers[i].classList.add("number-selected");
+      }
     }
+
+    eventTarget.appendChild(coin.cloneNode(true));
+    eventTarget.appendChild(removeCoinsBtn);
+    let coins = document.querySelectorAll(".coin");
+
+    coins.forEach((coin) => {
+      coin.style.left += coins.length + "1px";
+    });
+    removeCoinsBtn.addEventListener("click", () => {
+      for (let i = 0; i < coins.length; i++) {
+        coins[i].remove();
+
+        for (let i = 0; i < 13; i++) {
+          numbers[i].classList.remove("number-selected");
+        }
+        console.log(selectedNumbers);
+      }
+    });
+    console.log(selectedNumbers);
   }
-  // second add/remove items from array
-  if (!selectedNumbers.includes(firstTwelve)) {
-    selectedNumbers.unshift(firstTwelve); // displays on screen
-  } else {
-    selectedNumbers.splice(index, 1);
-  }
-  displaySelectedNumbers.textContent = "You have selected: " + selectedNumbers;
-};
-// 2nd 12
-const secondTwelveSelect = () => {
-  let index = selectedNumbers.indexOf(secondTwelve);
-  // first add the classes where needed
-  for (let i = 13; i < 25; i++) {
-    // get numbers displayed on screen (red & black)
-    if (!numbers[i].classList.contains("number-selected")) {
-      numbers[i].classList.add("number-selected");
-    } else {
-      numbers[i].classList.remove("number-selected");
-    }
-  }
-  // second add/remove items from array
-  if (!selectedNumbers.includes(secondTwelve)) {
-    selectedNumbers.unshift(secondTwelve); // displays on screen
-  } else {
-    selectedNumbers.splice(index, 1);
-  }
-  displaySelectedNumbers.textContent = "You have selected: " + selectedNumbers;
-};
-// 3rd 12
-const thirdTwelveSelect = () => {
-  let index = selectedNumbers.indexOf(thirdTwelve);
-  // first add the classes where needed
-  for (let i = 25; i < 37; i++) {
-    // get numbers displayed on screen (red & black)
-    if (!numbers[i].classList.contains("number-selected")) {
-      numbers[i].classList.add("number-selected");
-    } else {
-      numbers[i].classList.remove("number-selected");
-    }
-  }
-  // second add/remove items from array
-  if (!selectedNumbers.includes(thirdTwelve)) {
-    selectedNumbers.unshift(thirdTwelve); // displays on screen
-  } else {
-    selectedNumbers.splice(index, 1);
-  }
-  displaySelectedNumbers.textContent = "You have selected: " + selectedNumbers;
-};
-// first half
-const firstHalfBtnSelect = () => {
-  let index = selectedNumbers.indexOf(firstHalf);
-  for (let i = 1; i < 19; i++) {
-    if (!numbers[i].classList.contains("number-selected")) {
-      numbers[i].classList.add("number-selected");
-    } else {
-      numbers[i].classList.remove("number-selected");
-    }
-  }
-  // second add/remove items from array
-  if (!selectedNumbers.includes(firstHalf)) {
-    selectedNumbers.unshift(firstHalf); // displays on screen
-  } else {
-    selectedNumbers.splice(index, 1);
-  }
-  displaySelectedNumbers.textContent = "You have selected: " + selectedNumbers;
-};
-// second half
-const secondHalfBtnSelect = () => {
-  let index = selectedNumbers.indexOf(secondHalf);
-  for (let i = 19; i < 37; i++) {
-    if (!numbers[i].classList.contains("number-selected")) {
-      numbers[i].classList.add("number-selected");
-    } else {
-      numbers[i].classList.remove("number-selected");
-    }
-  }
-  // second add/remove items from array
-  if (!selectedNumbers.includes(secondHalf)) {
-    selectedNumbers.unshift(secondHalf); // displays on screen
-  } else {
-    selectedNumbers.splice(index, 1);
-  }
-  displaySelectedNumbers.textContent = "You have selected: " + selectedNumbers;
+  //
 };
 
-// Select numbers function for each number
-numbers.forEach((number) => number.addEventListener("click", selectNumber));
-// Start game function
-startGameBtn.addEventListener("click", startGame);
-firstTwelveBtn.addEventListener("click", firstTwelveSelect);
-secondTwelveBtn.addEventListener("click", secondTwelveSelect);
-thirdTwelveBtn.addEventListener("click", thirdTwelveSelect);
-//
-firstHalfBtn.addEventListener("click", firstHalfBtnSelect);
-secondHalfBtn.addEventListener("click", secondHalfBtnSelect);
+document.getElementById("startGameBtn").addEventListener("click", startGame);
+numbers.forEach((number) => {
+  number.addEventListener("click", selectSingleNumber);
+});
+buttons.forEach((button) => {
+  button.addEventListener("click", selectMultipleNumbers);
+});
